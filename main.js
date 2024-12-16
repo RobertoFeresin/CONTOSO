db453bc0c5f485cc5778915278397dc8e0d34c9d
RobertoFeresin <betotiniazevedo@gmail.com>
2024-12-16 11:17:18 -0300

    Commit funcionalidade para botões grid esquerda.

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-function toggleCollapse() {
-    const content = document.getElementById('demo');
-    content.classList.toggle('show');
+function toggleCollapse(sectionId) {
+    var content = document.getElementById(sectionId);
+    var allContents = document.querySelectorAll('.collapsible-content');
+    
+    allContents.forEach(function (el) {
+      if (el !== content) {
+        el.style.maxHeight = null;
+      }
+    });
+  
+    // Alternar a visibilidade do conteúdo clicado
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
   }
+  
