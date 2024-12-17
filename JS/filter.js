e94403f3019afb3cc29e7817d66eaf46e386d9e4
RobertoFeresin <betotiniazevedo@gmail.com>
2024-12-17 10:00:27 -0300

    Commit Fliter feito

--- JS/filter.js
+++ JS/filter.js
@@ -1,21 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
+  
+    const searchInput = document.getElementById('searchInput');
+    searchInput.addEventListener('input', function () {
+      const searchValue = this.value.toLowerCase().trim();
+      const graphs = document.querySelectorAll('.content-grid .grafico');
+    
+      graphs.forEach(graph => {
+        const title = graph.querySelector('h6').textContent.toLowerCase();
+        if (title.includes(searchValue)) {
+          graph.style.display = 'block'; 
+        } else {
+          graph.style.display = 'none'; 
+        }
+      });
+    });
+  
 
     document.querySelectorAll(".dropdown-item").forEach((item) => {
       item.addEventListener("click", function (e) {
-        e.preventDefault(); 
-  
+        e.preventDefault();
+    
         const value = this.getAttribute("data-value");
-  
+    
         const dropdownButton = this.closest(".dropdown").querySelector(".dropdown-toggle");
-  
+    
         dropdownButton.textContent = value;
       });
     });
   
+    
     const resetButton = document.getElementById("btnfilter");
     resetButton.addEventListener("click", function () {
-      // Resetar o campo de busca
-      const searchInput = document.getElementById("searchInput");
       searchInput.value = "";
   
       document.querySelectorAll(".dropdown-toggle").forEach((dropdown) => {
@@ -24,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
   
       const graphs = document.querySelectorAll(".content-grid .grafico");
       graphs.forEach((graph) => {
-        graph.style.display = "block";
+        graph.style.display = "block"; 
       });
     });
   });
