43e5cfc8a8130c242e28e2742a99d4d0a13a5a5c
RobertoFeresin <betotiniazevedo@gmail.com>
2024-12-17 09:56:07 -0300

    Adicionando funcionalidas no filter

--- JS/filter.js
+++ JS/filter.js
@@ -1,31 +1,30 @@
-document.getElementById('searchInput').addEventListener('input', function () {
-    const searchValue = this.value.toLowerCase().trim();
-    const graphs = document.querySelectorAll('.content-grid .grafico');
+document.addEventListener("DOMContentLoaded", function () {
 
-    graphs.forEach(graph => {
-      const title = graph.querySelector('h6').textContent.toLowerCase();
-      if (title.includes(searchValue)) {
-        graph.style.display = 'block'; 
-      } else {
-        graph.style.display = 'none'; 
-      }
-    });
-  });
-
-  document.addEventListener("DOMContentLoaded", function () {
-    // Selecionar todos os itens de dropdown
     document.querySelectorAll(".dropdown-item").forEach((item) => {
       item.addEventListener("click", function (e) {
-        e.preventDefault(); // Prevenir comportamento padrão do link
+        e.preventDefault(); 
   
-        // Obter o valor do item selecionado
         const value = this.getAttribute("data-value");
   
-        // Encontrar o botão de dropdown correspondente
         const dropdownButton = this.closest(".dropdown").querySelector(".dropdown-toggle");
   
-        // Atualizar o texto do botão com o valor selecionado
         dropdownButton.textContent = value;
+      });
+    });
+  
+    const resetButton = document.getElementById("btnfilter");
+    resetButton.addEventListener("click", function () {
+      // Resetar o campo de busca
+      const searchInput = document.getElementById("searchInput");
+      searchInput.value = "";
+  
+      document.querySelectorAll(".dropdown-toggle").forEach((dropdown) => {
+        dropdown.textContent = "All";
+      });
+  
+      const graphs = document.querySelectorAll(".content-grid .grafico");
+      graphs.forEach((graph) => {
+        graph.style.display = "block";
       });
     });
   });
